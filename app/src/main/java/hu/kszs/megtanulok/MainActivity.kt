a9d4db49da36365a5be710c87981e5585775168f
Kokas Sandor <lista@kszs.hu>
2021-01-05 18:10:40 +0100

    elso210105

---
+++ app/src/main/java/hu/kszs/megtanulok/MainActivity.kt
@@ -0,0 +1,11 @@
+package hu.kszs.megtanulok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
